ba56baffa2c88623ed67b69536a8aa5bdd2bd511
classa558-prog <classa558@gmail.com>
2026-07-17 11:11:09 +0400

    Add files via upload

---
+++ Congratualtions message.py
@@ -0,0 +1,9 @@
+name = "Agrim"
+Split_text = "Hi'I am'happy!"
+print(name.upper())
+print(name.lower())
+length = len(name)
+print(name[0:length])
+print(str(Split_text.split("'")[1]))
+print(Split_text[::-1])
+print(name + ", " + Split_text.replace("'", " "))
