699adc9e15d38e0e92830ba27a44ee375ccbb3a0
classa558-prog <classa558@gmail.com>
2026-07-23 09:59:33 +0400

    Add files via upload

---
+++ Summer_clothes.py
@@ -0,0 +1,13 @@
+Temperature = int(input("Input the temperature: "))
+if Temperature < 0:
+    print("You should wear double shirts an a big coat.")
+
+elif Temperature > 0: 
+    print("wear some longsleeves and some a fair jacket!")
+
+elif Temperature > 20:
+    print("Wear Some normal Clothes!")
+
+elif Temperature > 30:
+    print("It's really hot! Wear minimal light cotton clothes and stay indoors! ")
+
