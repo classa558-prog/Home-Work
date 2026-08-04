52ab277198672465ec2c3dbde5cd3a4a8d1e98b2
classa558-prog <classa558@gmail.com>
2026-08-04 11:44:57 +0400

    add Square_root_calculator.py

--- Square_root_calculator.py
+++ Square_root_calculator.py
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
+
 Number = int(input("Input a Integer:"))
 Root_power = int(input("which power root would you like; (e.g. 2 for square root or 3 for cube root):"))
 Result = Number**(1/Root_power)
-=======
-Number = int(input("Input a Integer:"))
-Root_power = int(input("which power root would you like; (e.g. 2 for square root or 3 for cube root):"))
-Result = Number**(1/Root_power)
->>>>>>> refs/remotes/origin/main
+
 print("The ", Root_power, "of ", Number, "is ", round(Result))
