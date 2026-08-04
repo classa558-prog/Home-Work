f5d9a87498924e051a9ac1101a3ce59e7fd741ca
classa558-prog <classa558@gmail.com>
2026-08-04 10:30:03 +0400

    add Binary_number_converyer.py add Square.py

---
+++ Square.py
@@ -0,0 +1,8 @@
+import turtle
+turtle.Screen().bgcolor("orange")
+turtle.Screen().setup(400, 300)
+Cursor = turtle.Turtle()
+for i in range(4):
+    Cursor.forward(60)
+    Cursor.right(90)
+turtle.done()
