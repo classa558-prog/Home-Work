06a48a76e4304dff8dadea434b720bc143a9fada
classa558-prog <classa558@gmail.com>
2026-08-04 11:50:57 +0400

    add Square.py

--- Square.py
+++ Square.py
@@ -1,4 +1,3 @@
-
 import turtle
 turtle.Screen().bgcolor("orange")
 turtle.Screen().setup(400, 300)
@@ -6,13 +5,4 @@ Cursor = turtle.Turtle()
 for i in range(4):
     Cursor.forward(60)
     Cursor.right(90)
-
-import turtle
-turtle.Screen().bgcolor("orange")
-turtle.Screen().setup(400, 300)
-Cursor = turtle.Turtle()
-for i in range(4):
-    Cursor.forward(60)
-    Cursor.right(90)
-
 turtle.done()
