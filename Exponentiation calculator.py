73e711feece67df39967c2cb834273986b43d75b
classa558-prog <classa558@gmail.com>
2026-08-04 11:49:29 +0400

    add Exponentiation calculator.py

--- Exponentiation calculator.py
+++ Exponentiation calculator.py
@@ -9,13 +9,5 @@ for i in range(exponent):
 
 print("Result:", result)
 
-base = int(input("Base: "))
-exponent = int(input("Exponent: "))
 
-result = 1
-
-for i in range(exponent):
-    result *= base
-
-print("Result:", result)
 
