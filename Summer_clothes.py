f576664eaf16b4e738b09bd1de2341894542ad82
classa558-prog <classa558@gmail.com>
2026-08-04 11:48:33 +0400

    add Summer_clothes.py

--- Summer_clothes.py
+++ Summer_clothes.py
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-Temperature = float(input("Input the temperature: "))
-if Temperature <= 0:
-    print("You should wear double shirts an a big coat.")
-
-elif Temperature >= 0: 
-    print("wear some longsleeves and some a fair jacket!")
-
-elif Temperature >= 20:
-    print("Wear Some normal Clothes!")
-
-elif Temperature >= 30:
-    print("It's really hot! Wear minimal light cotton clothes and stay indoors! ")
-
-else:
-    print("It's really hot! Wear minimal light cotton clothes and stay indoors! ")
-=======
 Temperature = int(input("Input the temperature: "))
 if Temperature < 0:
     print("You should wear double shirts an a big coat.")
@@ -28,4 +11,3 @@ elif Temperature > 20:
 elif Temperature > 30:
     print("It's really hot! Wear minimal light cotton clothes and stay indoors! ")
 
->>>>>>> refs/remotes/origin/main
