604c7dd3732d26022b052be6c12c1fce8d345afb
classa558-prog <classa558@gmail.com>
2026-08-04 11:51:51 +0400

    add Congratualtions message.py

--- Congratualtions message.py
+++ Congratualtions message.py
@@ -1,13 +1,3 @@
-
-name = "Agrim"
-Split_text = "Hi'I am'happy!"
-print(name.upper())
-print(name.lower())
-length = len(name)
-print(name[0:length])
-print(str(Split_text.split("'")[1]))
-print(Split_text[::-1])
-
 name = "Agrim"
 Split_text = "Hi'I am'happy!"
 print(name.upper())
