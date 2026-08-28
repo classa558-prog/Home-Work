dbbaae11324b32a357ccb9b696166e19c0426bf4
classa558-prog <classa558@gmail.com>
2026-08-28 08:39:56 +0400

    add Age_Counter.py

---
+++ Age_Counter.py
@@ -0,0 +1,20 @@
+try:
+    age = int(input("Enter your age: "))
+    if age < 0 or age < 18:
+        raise ArithmeticError
+    if age % 2 == 0:
+            print("Odd number and you are permitted for a license.")
+    else:
+        print("Even number and you are permitted for a license.")
+
+    
+except ValueError:
+    print("You have not entered an integer! ")
+except ArithmeticError:
+    print("Invalid age, cannot be lower than 0 or 18!")
+
+
+    
+
+
+
